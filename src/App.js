4b90cc1b6888b2abb613952f73a157011861d122
Grandon Smith <grandon.smith@yahoo.com>
2020-10-15 13:36:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip'
+
 
 function App() {
+
+  const firstTooltip = (
+    <Tooltip color='hotpink' message='tooltip message'>
+      ipsum
+    </Tooltip>
+  )
+  const secondTooltip = (
+    <Tooltip color='#126bcc' message='another tooltip message'>
+      ipsuminum
+    </Tooltip>
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='App'>
+      <Split className='left' flexBasis={2}>
+  This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, {firstTooltip} adipisicing elit. Incidunt ex velit suscipit facere officia?
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+  This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
+    </main>
+  )
 }
 
 export default App;
