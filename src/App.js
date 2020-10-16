cb120805be6cf3d2523c97bf50efe3efb4b70073
Grandon Smith <grandon.smith@yahoo.com>
2020-10-16 12:10:19 -0400

    just tryin to keep up

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import React from 'react';
-import Split from './composition/Split';
+import React, { Component } from 'react';
 import './App.css';
-import Tooltip from './composition/Tooltip'
+import Messages from './messages-app/Messages';
 
-
-function App() {
-
-  const firstTooltip = (
-    <Tooltip color='hotpink' message='tooltip message'>
-      ipsum
-    </Tooltip>
-  )
-  const secondTooltip = (
-    <Tooltip color='#126bcc' message='another tooltip message'>
-      ipsuminum
-    </Tooltip>
-  )
-  return (
-    <main className='App'>
-      <Split className='left' flexBasis={2}>
-  This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, {firstTooltip} adipisicing elit. Incidunt ex velit suscipit facere officia?
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
-      <Split className='right'>
-  This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-    </main>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>YOUR APPLICATION NAME!</h1>
+        <Messages name="Messages" unread={0}/>
+        <Messages name="Notifications" unread={10}/>
+      </div>
+    );
+  }
 }
 
 export default App;
