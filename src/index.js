a0a0b9170dc17a46819550ef816f7f73a70d5069
Grandon Smith <grandon.smith@yahoo.com>
2020-11-02 20:12:55 -0500

    Added toe product page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import {BrowserRouter} from 'react-router-dom'
+import App from './paltrows-power-toes/paltrows-power-toes-1-setup/src/App'
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
